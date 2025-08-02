5ba3a52e7854c8d2f12111b0e96dbfcd25c3b9aa
= <animesh7995@gmail.com>
2025-08-02 16:19:26 +0530

    fix minor

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -215,7 +215,7 @@ const Checkout = () => {
     return (
         <div className="min-h-screen flex flex-col bg-gradient-to-b from-green-50 via-white to-emerald-50">
             <Header isLoggedIn={isLoggedIn}/>
-            <main className="flex-grow px-3 py-3 flex justify-center items-start">
+            <main className="flex-grow px-3 py-3 justify-center items-start">
                 <h2 className="text-3xl font-bold text-center text-emerald-600 m-2">
                     🛒 Checkout
                 </h2>
